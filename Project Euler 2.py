01fc317aad7e1410a8ff33babde7f756e0fad3a9
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2019-04-17 14:07:07 +0300

    added full task code

--- Project Euler 2.py
+++ Project Euler 2.py
@@ -1,17 +1,18 @@
-n = int(input())
-def fibo(n):
+def task2(n):
     f = [0, 1]
     for i in range(2, n + 1):
-        f.append(f[i-1] + f[i-2])
+            f.append(f[i-1] + f[i-2])
+            if f[i] > 4000000:
+                del f[i]
+                break
     print(f)
-fibo(n)
-
-
-'''def task2 ():
-    a = []
-    b = 0
-    c = [i for i in range(1001)]
-    for i in range (1002):
-        a.append(a[i-1]+a[i+1])
+    a = 0
+    for i in range(len(f)):
+        if f[i]%2 == 0:
+            a += f[i]
     print(a)
-task2()'''
+
+
+task2(100)
+
+
